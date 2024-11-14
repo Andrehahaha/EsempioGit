f7751455686d2b3a649f10aa51095bb3af65abf5
andre <andre@pc>
2024-11-14 17:16:36 +0100

    Sovrascritto il println

--- src/main/java/esempiogit/Esempio.java
+++ src/main/java/esempiogit/Esempio.java
@@ -13,6 +13,6 @@ public class Esempio {
     public static void main (String args[]){
         System.out.println("Salve mondo!");
         System.out.println("Ciao");
-        System.out.println("Hello world!");
+        System.out.println("CIIII");
     }
 }
